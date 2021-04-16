2f1d5e521411726998b818f2858467ec69b94290
Brian Caffey <briancaffey2010@gmail.com>
2021-04-15 23:54:10 -0400

    refactor post form

--- backend/apps/blog/forms.py
+++ backend/apps/blog/forms.py
@@ -5,21 +5,19 @@ from apps.blog.models import Post
 
 class PostForm(ModelForm):
 
-    body = CharField(widget=Textarea)
+    body = CharField(
+        widget=Textarea(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Write your post here",
+                "id": "email",
+                "rows": 5,
+                "autofocus": True,
+                "type": "email",
+            }
+        )
+    )
 
     class Meta:
         model = Post
         fields = ["body"]
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields["body"].widget.attrs.update(
-            {
-                "class": "form-control",
-                "placeholder": "Write your post here",
-                "required": True,
-                "maxlength": 200,
-                "rows": 3,
-                "id": "post-body",
-            }
-        )
