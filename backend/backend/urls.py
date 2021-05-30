b3d220ec5d1502037a2ab5affb3eb2b6f9532ba8
Brian Caffey <briancaffey2010@gmail.com>
2021-05-30 18:13:48 -0400

    fix: move debug_toolbar import

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import debug_toolbar
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -50,6 +50,7 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:  # pragma: no cover
+    import debug_toolbar
     urlpatterns += [path("__debug__/", include(debug_toolbar.urls))] + static(
         settings.STATIC_URL,
         document_root=settings.STATIC_ROOT,
