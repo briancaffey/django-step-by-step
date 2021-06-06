9c28472231d6d859111557d3a1910bec0513f08a
Brian Caffey <briancaffey2010@gmail.com>
2021-06-06 15:37:36 -0400

    fix(secrets-manager): fix name of db secret name env var

--- backend/apps/core/utils.py
+++ backend/apps/core/utils.py
@@ -6,8 +6,8 @@ from aws_secretsmanager_caching import InjectKeywordedSecretString, SecretCache
 client = botocore.session.get_session().create_client('secretsmanager', region_name='us-east-1')
 cache = SecretCache(client=client)
 
-DB_SECRET_ID = os.environ.get("DB_SECRET_ID", "")
+DB_SECRET_NAME = os.environ.get("DB_SECRET_NAME", "")
 
-@InjectKeywordedSecretString(secret_id=DB_SECRET_ID, cache=cache, func_password='password')
+@InjectKeywordedSecretString(secret_id=DB_SECRET_NAME, cache=cache, func_password='password')
 def from_secret(func_password):
     return func_password
