4fa70aa2a51eb34555cfe0bbe03448f7503e5fe4
Brian Caffey <briancaffey2010@gmail.com>
2021-06-05 23:31:32 -0400

    fix(secrets-manager): remove unsused code

--- backend/backend/settings/production.py
+++ backend/backend/settings/production.py
@@ -7,15 +7,12 @@ DEBUG = 0
 
 STATIC_ROOT = "staticfiles/"
 
-# Secrets manager Secret ID passed in as environment variable
-# Use this to look up secret from Secret string
-SECRET_ID = os.environ.get("SECRET_ID")
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": os.environ.get("POSTGRES_NAME", "postgres"),
         "USER": os.environ.get("POSTGRES_USERNAME", "postgres"),
+        # TODO: rename this function
         "PASSWORD": from_secret(),
         "HOST": os.environ.get("POSTGRES_SERVICE_HOST", "localhost"),
         "PORT": os.environ.get("POSTGRES_SERVICE_PORT", 5432),
