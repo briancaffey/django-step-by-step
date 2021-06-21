a15f654a7afe74fbcd2d849582b213edc899da8a
Brian Caffey <briancaffey2010@gmail.com>
2021-06-20 23:04:45 -0400

    feat(dev): remove watchdog development dependency

---
+++ backend/apps/core/management/commands/start_beat.py
@@ -0,0 +1,30 @@
+"""
+https://avilpage.com/2017/05/how-to-auto-reload-celery-workers-in-development.html
+"""
+
+import os
+import shlex
+import subprocess
+
+from django.core.management.base import BaseCommand
+from django.utils import autoreload
+
+PIDFILE = '/code/celerybeat.pid'
+
+
+def restart_celery_beat():
+    cmd = 'pkill -9 celery'
+    subprocess.call(shlex.split(cmd))
+    cmd = f'celery --app=backend.celery_app:app beat --loglevel=info --pidfile={PIDFILE}' # noqa
+    subprocess.call(shlex.split(cmd))
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        print('Starting celery worker with autoreload...') # noqa
+        try:
+            os.remove('celerybeat.pid')
+        except FileNotFoundError as e:
+            print(e) # noqa
+        autoreload.run_with_reloader(restart_celery_beat)
