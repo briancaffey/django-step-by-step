9935b320785ea140aab470282c6f9ac16f066a1b
Brian Caffey <briancaffey2010@gmail.com>
2021-07-03 14:17:49 -0400

    revert name of setting AWS_STORAGE_BUCKET_NAME

--- backend/backend/settings/production.py
+++ backend/backend/settings/production.py
@@ -11,8 +11,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # media files
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-S3_BUCKET_NAME = os.environ["S3_BUCKET_NAME"]
-MEDIA_URL = f"http://{S3_BUCKET_NAME}.s3.amazonaws.com/"
+AWS_STORAGE_BUCKET_NAME = os.environ["S3_BUCKET_NAME"]
+MEDIA_URL = f"http://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/"
 AWS_PRIVATE_MEDIA_LOCATION = "media/private"
 PRIVATE_FILE_STORAGE = "backend.storage_backends.PrivateMediaStorage"
 AWS_STATIC_LOCATION = "static"
