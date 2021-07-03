0c3432e0d40314669a8df9a235836f66073c9358
Brian Caffey <briancaffey2010@gmail.com>
2021-07-03 00:55:18 -0400

    add whitenoise to poetry deps, export deps, configure whitenoise middleware

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
