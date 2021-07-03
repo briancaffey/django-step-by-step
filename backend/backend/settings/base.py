b5fc22832a47258b2d36466403fa9bf74472ce7f
Brian Caffey <briancaffey2010@gmail.com>
2021-07-03 14:47:34 -0400

    add django-cors-headers

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -28,6 +28,7 @@ DEBUG = bool(int(os.environ.get("DEBUG", "1")))
 
 ALLOWED_HOSTS = ["*"]
 
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
     # third-party
     "rest_framework",
     "graphene_django",
+    "corsheaders",
     # local apps
     "apps.accounts",
     "apps.core",
@@ -49,6 +51,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
