62d59d0c8ab249c5c92c55f9b7fd5cb1eb6a32c3
Brian Caffey <briancaffey2010@gmail.com>
2021-07-03 13:34:42 -0400

    add logging to heroku settings module

--- HEROKU.md
+++ HEROKU.md
@@ -123,3 +123,42 @@ heroku config:set SECRET_KEY=$SECRET_KEY
 ```
 git push heroku feature/post-images:main
 ```
+
+## LOGGING
+
+[https://stackoverflow.com/questions/18920428/django-logging-on-heroku](https://stackoverflow.com/questions/18920428/django-logging-on-heroku)
+
+```python
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'testlogger': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
+    }
+}
+```
--- backend/backend/settings/heroku.py
+++ backend/backend/settings/heroku.py
@@ -7,6 +7,47 @@ import dj_database_url
 
 DATABASES["default"] = dj_database_url.config()
 
+## Static files
+# https://stackoverflow.com/questions/11569144/proper-way-to-handle-static-files-and-templates-for-django-on-heroku
+
 # when deploying to heroku, pull in env vars for AWS access
 AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
 AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
+STATICFILES_STORAGE = "storages.backends.s3boto.S3BotoStorage"
+
+# necessary to fix manage.py collectstatic command to only upload changed files instead of all files
+AWS_PRELOAD_METADATA = True
+STATIC_URL = f"https://{S3_BUCKET_NAME}.s3.amazonaws.com/static/"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": (
+                "%(asctime)s [%(process)d] [%(levelname)s] "
+                + "pathname=%(pathname)s lineno=%(lineno)s "
+                + "funcname=%(funcName)s %(message)s"
+            ),
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+        "simple": {"format": "%(levelname)s %(message)s"},
+    },
+    "handlers": {
+        "null": {
+            "level": "DEBUG",
+            "class": "logging.NullHandler",
+        },
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        },
+    },
+    "loggers": {
+        "testlogger": {
+            "handlers": ["console"],
+            "level": "INFO",
+        }
+    },
+}
