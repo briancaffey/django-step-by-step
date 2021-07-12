64bce68357a6b269cc6fe6e1747e1efffa4be65f
Brian Caffey <briancaffey2010@gmail.com>
2021-07-11 23:43:36 -0400

    fix(cypress): add frontend url to cypress.json and clean up cypress test

--- cypress/cypress/integration/quasar/createPost.spec.js
+++ cypress/cypress/integration/quasar/createPost.spec.js
@@ -1,13 +1,14 @@
 describe("Test Quasar CRUD functionality", function () {
   it("can visit the homepage", function () {
-    cy.visit("http://localhost:8080/posts");
-    cy.contains("Vue");
+    cy.visit(Cypress.config("frontendUrl") + "/posts");
+    cy.contains("μblog");
   });
 
   it("can create a new post", function () {
-    cy.visit("http://localhost:8080/posts/new");
-    cy.get('[data-cy="post-textarea"]').type("This post was created on the Quasar app.");
+    cy.visit(Cypress.config("frontendUrl") + "/new-post");
+    const text = 'This post was created on the Quasar app.';
+    cy.get('[data-cy="post-textarea"]').type(text);
     cy.get('[data-cy="post-submit-btn"]').click();
-    cy.contains("Quasar");
+    cy.contains(text);
   });
 });
