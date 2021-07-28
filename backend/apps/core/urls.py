c4b1ffbb04bd63587bd8e1890f13bdb7efb06db6
Brian Caffey <briancaffey2010@gmail.com>
2021-07-27 22:02:27 -0400

    remove version view

--- backend/apps/core/urls.py
+++ backend/apps/core/urls.py
@@ -5,5 +5,4 @@ from apps.core import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("health-check/", views.health_check, name="health_check"),
-    path("version/", views.VersionView.as_view(), name="api-version"),
 ]
--- backend/apps/core/views.py
+++ backend/apps/core/views.py
@@ -1,9 +1,5 @@
-import subprocess
-
 from django.http import JsonResponse
 from django.shortcuts import render  # noqa
-from rest_framework.views import APIView
-from rest_framework import permissions
 
 
 def health_check(request):
@@ -12,25 +8,3 @@ def health_check(request):
 
 def index(request):
     return render(request, "index.html")
-
-
-class VersionView(APIView):
-    permission_classes = (permissions.AllowAny,)
-
-    def get(self, request, *args, **kwargs):
-
-        # get the current git tag, if there is no tag, fall back to
-        # the current git hash
-        git_tag = "-"
-        try:
-            git_tag = (
-                subprocess.check_output(
-                    ["git", "describe", "--tags", "--abbrev=0", "--always"]
-                )
-                .strip()
-                .decode()
-            )
-        except Exception:
-            git_tag = "-"
-
-        return JsonResponse({"git_tag": git_tag})
