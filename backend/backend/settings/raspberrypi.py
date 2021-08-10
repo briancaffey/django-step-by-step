6dbc884e15ded3aa909674dcf85541c22a213738
Brian Caffey <briancaffey2010@gmail.com>
2021-08-09 23:08:11 -0400

    set static and media root in raspi settings

--- backend/backend/settings/raspberrypi.py
+++ backend/backend/settings/raspberrypi.py
@@ -35,28 +35,6 @@ LOGGING = {
     },
 }
 
-# LOGGING = {
-#     "version": 1,
-#     "disable_existing_loggers": True,
-#     "formatters": {
-#         "verbose": {
-#             "format": "%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s"
-#         }
-#     },
-#     "handlers": {
-#         "gunicorn": {
-#             "level": "DEBUG",
-#             "class": "logging.handlers.RotatingFileHandler",
-#             "formatter": "verbose",
-#             "filename": "/opt/djangoprojects/reports/bin/gunicorn.errors",
-#             "maxBytes": 1024 * 1024 * 100,  # 100 mb
-#         }
-#     },
-#     "loggers": {
-#         "gunicorn.errors": {
-#             "level": "DEBUG",
-#             "handlers": ["gunicorn"],
-#             "propagate": True,
-#         },
-#     },
-# }
+STATIC_ROOT = "/code/staticfiles"
+
+MEDIA_ROOT = "/code/media"
