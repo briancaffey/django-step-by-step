51430836baa54c2567f39b87f735b0b7d4527656
Brian Caffey <briancaffey2010@gmail.com>
2021-09-05 01:14:26 -0400

    add notifications for login

--- quasar-app/src/modules/auth.ts
+++ quasar-app/src/modules/auth.ts
@@ -1,10 +1,17 @@
 import { ref, computed } from 'vue';
 import { api } from 'boot/axios';
+import { AxiosError } from 'axios';
+import { Notify } from 'quasar'
 import { useRouter } from 'vue-router';
 import useProfile from './profile';
 
+
 type TokenResponse = {
   access: string;
+}
+
+type ErrorResponse = {
+  detail: string;
 }
 
 const email = ref('');
@@ -55,21 +62,41 @@ export default function useAuth() {
   };
 
   const login = async (): Promise<void> => {
+    try {
+      const resp = await api.post<TokenResponse>('/api/auth/jwt/token/', {
+        email: email.value, password: password.value,
+      }, { withCredentials: true });
 
-    const resp = await api.post<TokenResponse>('/api/auth/jwt/token/', {
-      email: email.value, password: password.value,
-    }, { withCredentials: true });
+      accessToken.value = resp.data.access;
 
-    accessToken.value = resp.data.access;
+      await getProfile();
 
-    await getProfile();
+      setInterval(function () {
+        void refreshToken(false);
+      }, 1000 * 10);
 
-    setInterval(function () {
-      void refreshToken(false);
-    }, 1000 * 10);
+      // clear the login
+      email.value = '';
+      password.value = '';
 
-    await router.push('/');
+      await router.push('/');
 
+      Notify.create({
+        type: "positive",
+        message: "Welcome to μblog!",
+      });
+
+    } catch (err: any) {
+      // how do I type the error? AxiosError? any?
+      // https://github.com/axios/axios/blob/master/test/typescript/axios.ts
+
+      // this currently works but there is an unsafe access warning
+      // console.log(err.response.data.detail);
+      Notify.create({
+        type: 'warning',
+        message: err.response.data.detail,
+      });
+    }
   };
 
   return { login, refreshToken, email, password, accessToken, isAuthenticated, logout }
