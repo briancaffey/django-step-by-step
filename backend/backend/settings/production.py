9cad1030fe3842a700c76b2a784907d147ac8ac6
Brian Caffey <briancaffey2010@gmail.com>
2021-09-11 23:32:33 -0400

    fix media / static url in production settings

--- backend/backend/settings/production.py
+++ backend/backend/settings/production.py
@@ -9,9 +9,8 @@ INSTALLED_APPS += ("storages",)
 
 # static and media files
 AWS_STORAGE_BUCKET_NAME = os.environ["S3_BUCKET_NAME"]
-STATIC_ROOT = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/static/"
-MEDIA_ROOT = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/media/"
-MEDIA_URL = "/media/"
+STATIC_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/static/"
+MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/media/"
 AWS_PRIVATE_MEDIA_LOCATION = "media/private"
 PRIVATE_FILE_STORAGE = "backend.storage_backends.PrivateMediaStorage"
 AWS_STATIC_LOCATION = "static"
