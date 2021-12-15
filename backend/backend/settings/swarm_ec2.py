b8dcaf0f65dcefc9803b80c7f922647d7c743473
Brian Caffey <briancaffey2010@gmail.com>
2021-12-15 00:24:15 -0500

    add support for s3 static files

--- backend/backend/settings/swarm_ec2.py
+++ backend/backend/settings/swarm_ec2.py
@@ -2,3 +2,14 @@
 from .base import *
 
 DEBUG = False
+
+# when deploying to heroku, pull in env vars for AWS access
+AWS_DEFAULT_ACL = None
+STATICFILES_STORAGE = "backend.storage_backends.StaticStorage"
+
+# necessary to fix manage.py collectstatic command to only upload changed files instead of all files
+AWS_PRELOAD_METADATA = True
+S3_BUCKET_NAME = os.environ["S3_BUCKET_NAME"]
+AWS_S3_CUSTOM_DOMAIN = f"{S3_BUCKET_NAME}.s3.amazonaws.com"
+STATIC_ROOT = f"https://{S3_BUCKET_NAME}.s3.amazonaws.com/static/"
+MEDIA_ROOT = f"https://{S3_BUCKET_NAME}.s3.amazonaws.com/media/"
