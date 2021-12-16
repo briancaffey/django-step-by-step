963e357e919a5948c5e0a63ff312b728d974811a
Brian Caffey <briancaffey2010@gmail.com>
2021-12-16 01:19:38 -0500

    fix s3 configuration in django settings

--- backend/backend/settings/swarm_ec2.py
+++ backend/backend/settings/swarm_ec2.py
@@ -8,6 +8,11 @@ AWS_DEFAULT_ACL = None
 STATICFILES_STORAGE = "backend.storage_backends.StaticStorage"
 
 # necessary to fix manage.py collectstatic command to only upload changed files instead of all files
+AWS_STATIC_LOCATION = "static"
+
+AWS_PRIVATE_MEDIA_LOCATION = "media/private"
+PRIVATE_FILE_STORAGE = "backend.storage_backends.PrivateMediaStorage"
+
 AWS_PRELOAD_METADATA = True
 S3_BUCKET_NAME = os.environ["S3_BUCKET_NAME"]
 AWS_S3_CUSTOM_DOMAIN = f"{S3_BUCKET_NAME}.s3.amazonaws.com"
