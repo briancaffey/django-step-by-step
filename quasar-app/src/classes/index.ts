9d3bc61beaa8902ea1d0e033b436e8bacc389ad9
Brian Caffey <briancaffey2010@gmail.com>
2022-01-29 19:56:30 -0500

    feat(api-service): fix login and login routes in ApiService class

--- quasar-app/src/classes/index.ts
+++ quasar-app/src/classes/index.ts
@@ -1,4 +1,10 @@
+/**
+ * This file defines all backend REST API endpoint calls with axios
+ */
+
 import { api } from 'boot/axios';
+
+import { TokenResponse, LoginData, LoginResponse, LogoutResponse, LogoutData, RefreshResponse } from '../types'
 
 /**
  * class for making backend API calls
@@ -7,22 +13,57 @@ import { api } from 'boot/axios';
  */
 export default class ApiService {
 
+  // /**
+  //  * Activate a user account
+  //  *
+  //  * @param uidb64
+  //  * @param token
+  //  * @returns
+  //  */
+  // static activate(uidb64: string, token: string): Promise<unknown> {
+  //   return api.post(`/api/activate/${uidb64}/${token}/`);
+  // }
+
   /**
-   * Activate a user account
-   *
-   * @param uidb64
-   * @param token
-   * @returns
+   * Login a user
+   * @param {String} username
+   * @param {String} password
+   * @returns {String} Access token
    */
-  static activate(uidb64: string, token: string): Promise<unknown> {
-    return api.post(`/api/activate/${uidb64}/${token}/`);
+  async login(loginData: LoginData): Promise<LoginResponse> {
+    try {
+      const { data } = await api.post<TokenResponse>('/api/auth/jwt/token/', loginData, { withCredentials: true });
+      return [null, data];
+    } catch (error) {
+      console.error(error);
+      return [error]
+    }
   }
 
   /**
    * Logout
    */
-  static logout(): Promise<unknown> {
-    return api.post('/api/auth/jwt/token/logout/')
+  async logout(): Promise<LogoutResponse> {
+    try {
+      const { data } = await api.post<LogoutData>('/api/auth/jwt/token/logout/')
+      return [null, data]
+    } catch (error) {
+      console.error(error);
+      return [error]
+    }
+  }
+
+  /**
+   * Refresh token
+   */
+  async refreshToken(): Promise<RefreshResponse> {
+    try {
+      const { data } = await api.post<TokenResponse>('/api/auth/jwt/token/refresh/')
+      return [null, data]
+    } catch (error) {
+      console.error(error);
+      return [error]
+    }
   }
 
 }
---
+++ quasar-app/src/types/index.ts
@@ -0,0 +1,29 @@
+/**
+ * Types for API responses and other data
+ *
+ * https://stackoverflow.com/questions/37233735/interfaces-vs-types-in-typescript
+ */
+
+
+/**
+ * Interface for login data
+ */
+export interface LoginData {
+  email: string;
+  password: string;
+}
+
+export type TokenResponse = {
+  access: string;
+}
+
+export type LoginResponse = [null, TokenResponse] | [Error];
+
+
+export interface LogoutData {
+  message: string;
+};
+
+export type LogoutResponse = [null, LogoutData] | [Error];
+
+export type RefreshResponse = [null, TokenResponse] | [Error];
