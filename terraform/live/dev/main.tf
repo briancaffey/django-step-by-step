4dc2935007681b3de5492966b396c22930c5a49e
Brian Caffey <briancaffey2010@gmail.com>
2022-03-13 18:35:15 -0400

    add workflow for terraform init plan apply

---
+++ terraform/live/dev/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = ">=1.1.7"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+module "main" {
+  source              = "../../"
+  ecr_be_repo_url     = var.ecr_be_repo_url
+  ecr_fe_repo_url     = var.ecr_fe_repo_url
+  region              = var.region
+  acm_certificate_arn = var.acm_certificate_arn
+  instance_type       = var.instance_type
+  frontend_url        = var.frontend_url
+  zone_name           = var.zone_name
+  record_name         = var.record_name
+  force_destroy       = true
+  extra_env_vars = var.extra_env_vars
+}
