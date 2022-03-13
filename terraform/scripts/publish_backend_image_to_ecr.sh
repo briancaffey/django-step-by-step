8e1dd97a9f839dfa172723903cdcd3dfee59c1bf
Brian Caffey <briancaffey2010@gmail.com>
2022-03-13 13:44:18 -0400

    add job for building docker image from source code git tag

--- terraform/scripts/publish_backend_image_to_ecr.sh
+++ terraform/scripts/publish_backend_image_to_ecr.sh
@@ -6,7 +6,7 @@ echo "IMAGE_TAG is ${IMAGE_TAG}"
 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com
 
-docker build -t backend ./backend
+docker build -t backend:$IMAGE_TAG ./backend
 
 docker tag backend:$IMAGE_TAG $ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/backend:$IMAGE_TAG
 
--- terraform/scripts/publish_frontend_image_to_ecr.sh
+++ terraform/scripts/publish_frontend_image_to_ecr.sh
@@ -6,7 +6,7 @@ echo "IMAGE_TAG is ${IMAGE_TAG}"
 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com
 
-docker build -t frontend -f nginx/ecs/Dockerfile --progress=plain --no-cache .
+docker build -t frontend:$IMAGE_TAG -f nginx/ecs/Dockerfile --progress=plain --no-cache .
 
 docker tag frontend:$IMAGE_TAG $ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/frontend:$IMAGE_TAG
 
