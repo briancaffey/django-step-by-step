c55e1e187118d0ecd52bf88cd3e40bc388772102
Brian Caffey <briancaffey2010@gmail.com>
2022-03-14 02:21:34 -0400

    bump version of terraform-aws-django module

--- terraform/live/dev/main.tf
+++ terraform/live/dev/main.tf
@@ -17,7 +17,7 @@ provider "aws" {
 
 module "main" {
   source              = "briancaffey/django/aws"
-  version             = "0.2.0"
+  version             = "0.3.0"
   ecr_be_repo_url     = var.ecr_be_repo_url
   ecr_fe_repo_url     = var.ecr_fe_repo_url
   region              = var.region
