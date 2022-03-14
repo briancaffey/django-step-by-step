4d0bac7cbd9ae883b94d32a421b4bc1f53d3c289
Brian Caffey <briancaffey2010@gmail.com>
2022-03-13 21:02:20 -0400

    add workflow for updating frontend using tf outputs

--- terraform/live/dev/outputs.tf
+++ terraform/live/dev/outputs.tf
@@ -9,3 +9,15 @@ output "collectstatic_script" {
 output "app_url" {
   value = module.main.app_url
 }
+
+output "frontend_task_family" {
+  value = module.main.frontend_task_family
+}
+
+output "frontend_service_name" {
+  value = module.main.frontend_service_name
+}
+
+output "ecs_cluster_name" {
+  value = module.main.ecs_cluster_name
+}
