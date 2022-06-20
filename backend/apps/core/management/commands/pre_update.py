34480367ac05a3ac4e4ab206125442e4c1bf439b
briancaffey <briancaffey2010@gmail.com>
2022-06-19 22:42:45 -0400

    add pre_update management command

---
+++ backend/apps/core/management/commands/pre_update.py
@@ -0,0 +1,22 @@
+"""
+This commands calls other Django management commands to perform pre-update tasks
+during the update process.
+
+This may include:
+
+    - applying migrations to database with `migrate`
+    - `collectstatic`
+    - loading fixtures
+    - clearing cache (redis)
+"""
+
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+
+        # collectstatic
+        call_command("collectstatic", "--no-input")
+        # migrate
+        call_command("migrate")
