856c2c6652d386bec7cd646b3ab959b171177f9e
briancaffey <briancaffey2010@gmail.com>
2022-08-06 16:14:54 -0400

    configure k6 to use ramping-arrival-rate scenario

--- k6/script.js
+++ k6/script.js
@@ -3,8 +3,39 @@ import http from 'k6/http';
 const baseUrl = __ENV.BASE_URL || 'http://0.0.0.0:8000';
 
 export const options = {
-  vus: 10,
-  duration: '10s',
+  scenarios: {
+    contacts: {
+      // https://k6.io/docs/using-k6/scenarios/executors/ramping-arrival-rate
+      executor: 'ramping-arrival-rate',
+
+      // Our test with at a rate of 300 iterations started per `timeUnit` (e.g minute).
+      startRate: 300,
+
+      // It should start `startRate` iterations per minute
+      timeUnit: '1m',
+
+      // It should preallocate 2 VUs before starting the test.
+      preAllocatedVUs: 2,
+
+      // It is allowed to spin up to 50 maximum VUs in order to sustain the defined
+      // constant arrival rate.
+      maxVUs: 50,
+
+      stages: [
+        // It should start 300 iterations per `timeUnit` for the first minute.
+        { target: 300, duration: '1m' },
+
+        // It should linearly ramp-up to starting 600 iterations per `timeUnit` over the following two minutes.
+        { target: 600, duration: '2m' },
+
+        // It should continue starting 600 iterations per `timeUnit` for the following four minutes.
+        { target: 600, duration: '4m' },
+
+        // It should linearly ramp-down to starting 60 iterations per `timeUnit` over the last two minute.
+        { target: 60, duration: '2m' },
+      ],
+    },
+  },
 };
 
 export default function () {
