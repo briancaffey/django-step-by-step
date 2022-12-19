e003b4c65e0bff9275d638209bfcd16401edc15c
briancaffey <briancaffey2010@gmail.com>
2022-12-18 23:27:31 -0500

    feat(cdk): bump cdk version

--- iac/cdk/.projenrc.js
+++ iac/cdk/.projenrc.js
@@ -4,7 +4,7 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   defaultReleaseBranch: 'main',
   name: 'cdk',
 
-  deps: ['cdk-django'], /* Runtime dependencies of this module. */
+  deps: ['cdk-django@0.0.17'], /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
