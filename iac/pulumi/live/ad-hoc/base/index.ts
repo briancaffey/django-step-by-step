1516b18f22640d11871a9c6acea684f12fd9a8d4
briancaffey <briancaffey2010@gmail.com>
2022-12-30 01:30:48 -0500

    feat(pulumi): add ad hoc base iac code

--- iac/pulumi/live/ad-hoc/base/index.ts
+++ iac/pulumi/live/ad-hoc/base/index.ts
@@ -1,9 +1,19 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
+import { AdHocBaseEnvComponent } from 'pulumi-aws-django';
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+const adHocBaseEnv = new AdHocBaseEnvComponent('myAdHocEnv', {
+  certificateArn: process.env.CERTIFICATE_ARN || 'arn:aws:acm:us-east-1:111111111111:certificate/11111111-1111-1111-1111-111111111111',
+  domainName: process.env.DOMAIN_NAME || 'example.com'
+});
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+export const vpcId = adHocBaseEnv.vpc.vpcId;
+export const assetsBucket = adHocBaseEnv.assetsBucket.id;
+export const privateSubnetIds = adHocBaseEnv.vpc.privateSubnetIds;
+export const appSgId = adHocBaseEnv.appSecurityGroup.id;
+export const albSgId = adHocBaseEnv.albSecurityGroup.id;
+export const listenerArn = adHocBaseEnv.listener.arn;
+export const albDnsName = adHocBaseEnv.alb.dnsName;
+export const serviceDiscoveryNamespaceId = adHocBaseEnv.serviceDiscoveryNamespace.id;
+export const rdsAddress = adHocBaseEnv.databaseInstance.address;
+export const domainName = adHocBaseEnv.domainName;
+// export const baseStackName = adHocBaseEnv.stackName;
+// export const bastionHostInstanceId = adHocBaseEnv.bastionHostInstanceId;
