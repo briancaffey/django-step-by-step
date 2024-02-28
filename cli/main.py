a8108e970bff1e51a4b496ebcf0fcc57a14a0105
briancaffey <briancaffey2010@gmail.com>
2024-02-28 18:03:24 -0500

    feat(cli): update cli command

--- cli/main.py
+++ cli/main.py
@@ -1,13 +1,65 @@
-import boto3
+import argparse
 import subprocess
+
+import boto3
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="")
+
+    parser.add_argument(
+        "--base_env_name",
+        type=str,
+        required=True,
+        default=None,
+        help="Path to the R1 file"
+    )
+
+    parser.add_argument(
+        "--app_env_name",
+        type=str,
+        required=True,
+        default=None,
+        help="Number of threads to use"
+    )
+
+    args = parser.parse_args()
+    return args
 
 
 def main():
+    """
+    This command is used to get access to a running container with ecs-exec
+
+    This command should be IaC agnostic, so instead of using stack outputs
+    it will use tags to get resources
+
+    Items to look up via tags
+
+    * cluster name
+    * task id
+
+    arguments:
+
+    * base_env_name
+    * app_env_name
+    """
+
+    args = parse_args()
+
+    # use args to look up ecs cluster name
+    # base_env_name = args.base_env_name
+    app_env_name = args.app_env_name
+
+    # use aws cli get the ecs cluster name with tags app-env = app_env_name and base-env = base_env_name
+    cluster_name = f"{app_env_name}-cluster"
+
+    ecs_client = boto3.client("ecs")
+    task_id = ecs_client.list_tasks(cluster=cluster_name, serviceName=f"{app_env_name}-ecs-exec")["taskArns"][0]
 
     aws_command = [
         'aws', 'ecs', 'execute-command',
-        '--cluster', 'alpha-cluster',
-        '--task', '',
+        '--cluster', cluster_name,
+        '--task', task_id,
         '--command', 'bash',
         '--interactive'
     ]
