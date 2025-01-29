081fde42de2f277b68f95aa68cc0e77ef0668e88
Brian Caffey <briancaffey2010@gmail.com>
2025-01-29 22:30:43 +0700

    fix(typo): fix typo in terraform module

--- iac/terraform/live/ad-hoc/base/main.tf
+++ iac/terraform/live/ad-hoc/base/main.tf
@@ -1,8 +1,8 @@
 module "main" {
   # for local development
-  source          = "../../../../../../terraform-aws-django/modules/ad-hoc/base"
+  # source          = "../../../../../../terraform-aws-django/modules/ad-hoc/base"
   # for production use use git URL pointing to the module (recommended to use tags instead of main branch)
-  # source          = "git::https://github.com/briancaffey/terraform-aws-django.git//modules/ad-hoc/base"
+  source          = "git::https://github.com/briancaffey/terraform-aws-django.git//modules/ad-hoc/base" # add ?ref=<branch> to use a branch
   certificate_arn = var.certificate_arn
   domain_name     = var.domain_name
 }
