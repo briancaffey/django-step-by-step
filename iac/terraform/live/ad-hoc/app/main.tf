8bf72a0c9e95b68094fc805fcfe7a1c279afdd3d
Brian Caffey <briancaffey2010@gmail.com>
2025-01-30 15:17:14 +0700

    fix(tf): fix source value to point to git repo rather than local dir

--- iac/terraform/live/ad-hoc/app/main.tf
+++ iac/terraform/live/ad-hoc/app/main.tf
@@ -16,7 +16,9 @@ data "terraform_remote_state" "this" {
 # main
 
 module "main" {
-  source                         = "../../../../../../terraform-aws-django/modules/ad-hoc/app"
+  # uncomment for easy local development and testing
+  # source                         = "../../../../../../terraform-aws-django/modules/ad-hoc/app"
+  source                         = "git::https://github.com/briancaffey/terraform-aws-django.git//modules/ad-hoc/base" # add ?ref=<branch> to use a branch
   vpc_id                         = data.terraform_remote_state.this.outputs.vpc_id
   assets_bucket_name             = data.terraform_remote_state.this.outputs.assets_bucket_name
   private_subnet_ids             = data.terraform_remote_state.this.outputs.private_subnet_ids
