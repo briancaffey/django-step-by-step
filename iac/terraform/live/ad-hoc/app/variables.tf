526fc1a69f700ffe8c0a6b605c53037f5bb7a157
Brian Caffey <briancaffey2010@gmail.com>
2025-01-30 16:27:00 +0700

    fix(tf): add variable for rds secret name

--- iac/terraform/live/ad-hoc/app/variables.tf
+++ iac/terraform/live/ad-hoc/app/variables.tf
@@ -193,3 +193,8 @@ variable "fe_image_tag" {
 #   description = "Amount (in MiB) of memory used by the task"
 #   type        = number
 # }
+
+variable "rds_password_secret_name" {
+  type = string
+  description = "AWS Secrets Manager secret name used for RDS instance"
+}
