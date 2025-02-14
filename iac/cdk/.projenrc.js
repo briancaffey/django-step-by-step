c6df6e442cc3043512e10120c29b14150fd2d14e
Brian Caffey <briancaffey2010@gmail.com>
2025-02-14 14:20:02 +0700

    fix(cdk): bump cdk-django version for dependency fix

--- iac/cdk/.projenrc.js
+++ iac/cdk/.projenrc.js
@@ -1,10 +1,10 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.178.1',
+  cdkVersion: '2.178.2',
   defaultReleaseBranch: 'main',
   name: 'cdk',
 
-  deps: ['cdk-django@1.7.0'], /* Runtime dependencies of this module. */
+  deps: ['cdk-django@1.8.0'], /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
