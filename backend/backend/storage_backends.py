9a187867243442060f1e73c6b89f97f29ea1e3ac
Brian Caffey <briancaffey2010@gmail.com>
2025-02-14 16:38:17 +0700

    fix(storage): remove default_acl setting on static file storage

--- backend/backend/storage_backends.py
+++ backend/backend/storage_backends.py
@@ -5,7 +5,6 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
     location = settings.AWS_STATIC_LOCATION
-    default_acl = "public-read"
 
 
 if settings.DEBUG:
